cfc14f254d91cea72e94536a52e2bce7ddc58685
Radka Krenova <radka.krenov@gmail.com>
2022-08-28 00:28:17 +0200

    js

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ console.log(a); //This will output 10 into the console
 /*
 This is a multi-line comment
 */
+
+
+
+//2
+
+
+let str1 = "cat";
+let str2 = "dog";
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+
+console.log(typeof(str1));
+console.log(typeof(str2));
+console.log(typeof(val1));
+console.log(typeof(val2));
+console.log(typeof(myNum));
+
+
+const myName = "Radka";
+const myAge = 33;
+const coder = true;
+const message = "Hello, my name is " + myName + ", I am " + myAge + " years old and I can code JavaScript: " + coder + "."
+console.log(message);
